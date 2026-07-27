1d4f31f3529a4a797a0caad85bfb6476e0bc91de
GabrielLFM <biel.lucianomelo@gmail.com>
2026-07-27 11:07:49 -0300

    initial commit

---
+++ src/main/java/com/devcore/medup/MedupApplication.java
@@ -0,0 +1,13 @@
+package com.devcore.medup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devcore/medup/MedupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcore.medup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
